9591be85d4f721e5cda05d5ed78fc70e5aa35b23
Žiga Miklošič <ziga.miklosic@seltron.si>
2022-11-23 09:12:07 +0100

    - change slow loop period back to 1 second

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -41,7 +41,7 @@ MAIN_WIN_FAST_TIM_PERIOD    = 50
 # Slow timer period
 #
 # Unit: ms
-MAIN_WIN_SLOW_TIM_PERIOD    = 500
+MAIN_WIN_SLOW_TIM_PERIOD    = 1000
 
 # Serial command end symbol
 MAIN_WIN_COM_STRING_TERMINATION = "\r\n"
